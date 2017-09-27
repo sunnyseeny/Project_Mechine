01a461bec6aee41b2c0934be65dafad4abf74b21
Jakkapan <sunny_seeny@hotmail.com>
2017-09-27 18:05:30 +0700

    #1

---
+++ V1/V1.ino
@@ -0,0 +1,11 @@
+#include "motorpin.h"
+
+void setup() {
+  pinMode(IN
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ V1/motorpin.h
@@ -0,0 +1,16 @@
+//Motor1
+#define IN1
+#define IN2
+#define ENA
+
+//Motor2
+#define IN3
+#define IN4
+#define ENB
+
+//Motor3
+#define IN5
+#define IN6
+#define ENC
+
+
