e59506a6ab16d8f8129c832a2a89d582b10f2cec
Jakkapan <sunny_seeny@hotmail.com>
2017-09-29 22:54:52 +0700

    #4

---
+++ TESTBT/TESTBT.ino
@@ -0,0 +1,63 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BT(10, 11); //Rx,Tx
+unsigned int num[4] = {0, 0, 0, 0};
+void setup() {
+  Serial.begin(9600);
+  Serial.println("start");
+  BT.begin(9600);
+}
+
+void loop() {
+  static unsigned int cnt = 0;
+  if (BT.available()) {
+    int x = BT.read();
+    if (x >= 14) {
+      if (cnt % 4 == 0) num[0] = x - 48;
+      if (cnt % 4 == 1) num[1] = x - 48;
+      if (cnt % 4 == 2) num[2] = x - 48;
+      if (cnt % 4 == 3) num[3] = 0;
+      cnt++;
+    }
+    if (x == 11) {
+      if (cnt == 3) {
+        unsigned int rpml = num[0] * 100 + num[1] * 10 + num[2];
+        Serial.print("rpml= ");
+        Serial.println(rpml);
+      }
+      if (cnt == 2) {
+        unsigned int rpml = num[0] * 10 + num[1];
+        Serial.print("rpml= ");
+        Serial.println(rpml);
+      }
+      if (cnt == 1) {
+        unsigned int rpml = num[0];
+        Serial.print("rpml= ");
+        Serial.println(rpml);
+      }
+      cnt = 0;
+      x = 0;
+    }
+    else if (x == 12) {
+      if (cnt == 3) {
+        unsigned int rpmr = num[0] * 100 + num[1] * 10 + num[2];
+        Serial.print("rpmr= ");
+        Serial.println(rpmr);
+      }
+      if (cnt == 2) {
+        unsigned int rpmr = num[0] * 10 + num[1];
+        Serial.print("rpmr= ");
+        Serial.println(rpmr);
+      }
+      if (cnt == 1) {
+        unsigned int rpmr = num[0];
+        Serial.print("rpml= ");
+        Serial.println(rpmr);
+      }
+      cnt = 0;
+      x = 0;
+    }
+    Serial.print("Number= ");
+    Serial.println(x);
+  }
+
+}
